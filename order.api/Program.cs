96a3ed8c3fcf38284dc4173b6b8aa87523035414
vitou hun <vitouaj68@gmail.com>
2024-01-18 12:00:27 +0700

    first commit

---
+++ order.api/Program.cs
@@ -0,0 +1,21 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/order", () =>
+{
+    return "this is order api";
+});
+
+app.Run();
